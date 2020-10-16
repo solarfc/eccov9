5208093445453856b2743a985c6695509baaacb3
vladyslav1243 <galich.vinfort@gmail.com>
2020-10-16 17:27:45 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var myWidth = window.innerWidth,
+    myHeight = window.innerHeight;
+console.log("width ".concat(myWidth, " \n height ").concat(myHeight));
+
+window.onload = function () {
+  /*
+      fancybox loop
+   */
+  $.fancybox.defaults.loop = true;
+  /*
+      form styler
+   */
+
+  $(function () {
+    $('select').styler({
+      selectSmartPositioning: false
+    });
+  }); // /*
+  //     change href on mobile
+  //  */
+  //
+  // if(/iPhone|iPod|Android/i.test(navigator.userAgent)){
+  //     document.querySelector('a.grande').href = '#formgrande';
+  //     document.querySelector('a.lake').href = '#formlake';
+  //     document.querySelector('a.lou').href = '#formlou';
+  // }
+};
---
+++ app/script/main.js
@@ -0,0 +1,32 @@
+let myWidth = window.innerWidth,
+    myHeight = window.innerHeight;
+console.log(`width ${myWidth} \n height ${myHeight}`);
+
+window.onload = function () {
+
+    /*
+        fancybox loop
+     */
+
+    $.fancybox.defaults.loop = true;
+
+    /*
+        form styler
+     */
+
+    $(function () {
+        $('select').styler({
+            selectSmartPositioning: false
+        });
+    });
+
+    // /*
+    //     change href on mobile
+    //  */
+    //
+    // if(/iPhone|iPod|Android/i.test(navigator.userAgent)){
+    //     document.querySelector('a.grande').href = '#formgrande';
+    //     document.querySelector('a.lake').href = '#formlake';
+    //     document.querySelector('a.lou').href = '#formlou';
+    // }
+};
